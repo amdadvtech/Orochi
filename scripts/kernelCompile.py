c8d4a13c8ec21501160bf0fa45bb4d31f1cba25e
Atsushi.Yoshimura <Atsushi.Yoshimura@amd.com>
2023-02-07 19:58:39 +0900

    [ORO-0] add automatic arch selection

--- scripts/kernelCompile.py
+++ scripts/kernelCompile.py
@@ -1,7 +1,24 @@
 import json
 import os
 import subprocess
+import re
 
+def toNumber( arch ):
+    return int(arch[3:], 16)
+
+def enumArch( minArch ):
+    process = subprocess.Popen(['llc', '-march=amdgcn', '-mcpu=help'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+    output, errors = process.communicate()
+    lines = output.decode('utf-8').splitlines() + errors.decode('utf-8').splitlines()
+
+    arches = []
+    for line in lines:
+        result = re.match("\s+(gfx[0-9a-f]+).*processor.", line)
+        if result:
+            arch = result.group(1)
+            if toNumber(minArch) <= toNumber(arch):
+                arches.append( arch )
+    return arches
 
 def getGpuList():
   f = open("amdGpuList.json")
@@ -16,7 +33,7 @@ def compile( index ):
 			"..\\..\\hipsdk\\bin\\hipcc",
 			"-x", "hip", "..\ParallelPrimitives\RadixSortKernels.h", "-O3", "-std=c++17", "-ffast-math", "--cuda-device-only", "--genco", "-I../", "-include", "hip/hip_runtime.h", "-parallel-jobs=15"]
 		#command.append( "--offload-arch=gfx1100" )
-		for i in getGpuList()['amd']:
+		for i in enumArch( "gfx900" ):
 			command.append( "--offload-arch=" + i )
 		command.append( "-o" )
 		command.append( "../bitcodes/oro_compiled_kernels.hipfb" )
